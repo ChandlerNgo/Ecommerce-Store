4caef10698a3b2538029c9e37ed39a1b149f6f07
ChandlerNgo <chandlerngo00@gmail.com>
2022-07-31 17:11:31 -0500

    new changes

---
+++ app.py
@@ -0,0 +1,17 @@
+import falcon
+
+
+# Falcon follows the REST architectural style, meaning (among
+# other things) that you think in terms of resources and state
+# transitions, which map to HTTP verbs.
+class ThingsResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
+        resp.text = (
+            '\nTwo things awe me most, the starry sky '
+            'above me and the moral law within me.\n'
+            '\n'
+            '    ~ Immanuel Kant\n\n'
+        )
---
+++ wsgi.py
@@ -0,0 +1,14 @@
+from wsgiref.simple_server import make_server
+import falcon
+from app import ThingsResource
+
+app = falcon.App()
+
+app.add_route('/things', ThingsResource())
+
+if __name__ == '__main__':
+    with make_server('', 8000, app) as httpd:
+        print('Serving on port 8000...')
+
+        # Serve until process is killed
+        httpd.serve_forever()
